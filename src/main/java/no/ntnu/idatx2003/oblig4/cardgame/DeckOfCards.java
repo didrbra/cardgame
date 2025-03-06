e6b3deea5eaa6a50f2d8f6656555c060c8236d02
didrik <didrikcb@stud.ntnu.no>
2025-03-06 08:33:41 +0100

    i

---
+++ src/main/java/no/ntnu/idatx2003/oblig4/cardgame/DeckOfCards.java
@@ -0,0 +1,4 @@
+package no.ntnu.idatx2003.oblig4.cardgame;
+
+public class DeckOfCards {
+}
---
+++ src/main/java/no/ntnu/idatx2003/oblig4/cardgame/main.java
@@ -0,0 +1,5 @@
+package no.ntnu.idatx2003.oblig4.cardgame;
+
+public class main {
+    public static void main(String[] args) {}
+}
